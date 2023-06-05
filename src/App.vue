806c52e7543f9cb354677efbc9f317622d836f16
Naya <naya252naya@gmail.com>
2023-06-05 16:39:19 +0300

    create header and side-bar

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,68 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  components: {
+    // HelloWorld
+  },
+  data() {
+    return {
+      drawer: false,
+      group: true,
+      items: [
+        {
+          title: '1',
+          value: 1,
+          props: {
+            prependIcon: 'mdi-home',
+            to: '/'
+          }
+        },
+        {
+          title: '2',
+          value: 2,
+          props: {
+            prependIcon: 'mdi-home',
+            to: '/about'
+          }
+        }
+      ]
+    }
+  },
+  watch: {
+    group: function () {
+      this.drawer = true
+    }
+  }
+})
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <v-app>
+    <v-app-bar color="primary" density="compact">
+      <template v-slot:prepend>
+        <v-app-bar-nav-icon density="compact" @click="drawer = !drawer" />
+      </template>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <v-app-bar-title>Style management</v-app-bar-title>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+      <template v-slot:append>
+        <v-btn icon="mdi-dots-vertical" density="compact"></v-btn>
+      </template>
+    </v-app-bar>
 
-  <RouterView />
+    <v-navigation-drawer v-model="drawer" disable-route-watcher disable-resize-watcher>
+      <v-list
+        nav
+        :items="items"
+        item-title="title"
+        item-value="value"
+        active-color="primary"
+      ></v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <RouterView />
+    </v-main>
+  </v-app>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
